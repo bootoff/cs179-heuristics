2673a629c961eaf4f9775b8a5794e696cf852f50
ciyer64 <curreniyer@college.harvard.edu>
2016-04-20 17:32:31 -0400

    updates

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
+//Login
+var username;
+var password;
+
+username = "Curren"
+
 //Adding new comments
 var text;
 var posts = {val: 0, postList: []};
 
 $( document ).ready(function() {
-  if (localStorage.getItem("savedComments")) {
-    posts = JSON.parse(localStorage.getItem("savedComments"));
+  if (localStorage.getItem("oldCom")) {
+    posts = JSON.parse(localStorage.getItem("oldCom"));
   }
     for (var j=0;j<posts.postList.length; j++){
-      comment= "<li class='note' data-index='" + posts.postList[j].ind + "'> <p>" + posts.postList[j].content + "</p> </li>";
+      comment= "<li class='note' data-index='" + posts.postList[j].ind + "'> <p>" + posts.postList[j].user + ": " + posts.postList[j].content + "</p> </li>";
       $("ul").append(comment);
     }
   posts.val = posts.postList.length
 
   $(document).on('click', '.postbutton', function() {
     text = $(".newpost").val();
-    posts.postList.push({ind: posts.val, content: text});
-    var posttext = "<li class='post' data-index='" + posts.val + "'> <p>" + text + "</p> </li>";
+    author=username;
+    posts.postList.push({ind: posts.val, user: author, content: text});
+    var posttext = "<li class='post' data-index='" + posts.val + "'> <p>" + author + ": " + text + "</p> </li>";
     $("#postList").append(posttext);
     //$("#postList").append("<li>test 1</li>");
     console.log($("#postList"))
     posts.val++;
     console.log(posts);
-    localStorage.setItem("savedComments", JSON.stringify(posts));
+    localStorage.setItem("oldCom", JSON.stringify(posts));
 
   });
 
@@ -36,10 +43,10 @@ $( document ).ready(function() {
   //     if ($(this).attr("data-index") == posts.postList[k].ind){
   //       console.log("hello");
   //       posts.postList.splice(k,1);
-  //       localStorage.setItem("savedComments", JSON.stringify(posts));
+  //       localStorage.setItem("oldCom", JSON.stringify(posts));
   //     }
   //   }
-  //   localStorage.setItem("savedComments", JSON.stringify(posts));
+  //   localStorage.setItem("oldCom", JSON.stringify(posts));
   // });
 
 });
