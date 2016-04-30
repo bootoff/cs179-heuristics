f274a5d1670ab3d381b0f2335a052dfc5dd0469e
Catherine Bousquet <catherinebousquet@college.harvard.edu>
2016-04-30 15:32:27 -0400

    formatted category page for new goals

---
+++ temp/login.js
@@ -0,0 +1,18 @@
+function validate(){
+  var un = document.getElementById("username").value;
+  var pw = document.getElementById("password").value;
+  var unArray = ["ciyer", "alafler", "cbousquet"];
+  var pwArray = ["gajos", "bernd", "cs179"];
+
+  for (var i=0;i<unArray.length;i++){
+    if((pw==pwArray[i]) && (un==unArray[i])){
+      alert("Login successful");
+      window.location = "main.html"
+      return false;
+    }
+    else{
+      alert("Please try again");
+      return false;
+    }
+  }
+}
